aaff4a394d5fdff1573016b6511d953547db067f
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2018-09-14 16:44:45 -0400

    PaulsConsultingTest

---
+++ WebSitePlayGround/WebSitePlayGround/default.js
@@ -0,0 +1,14 @@
+﻿function add(num1, num2) {
+	return num1 + num2;
+}
+function changeText() {
+	// Use .textContent instead of .innerHTML for security reasons
+	// and instead of .innerText due to some layout system information
+	// that causes performance decreases
+	document.getElementById("clickChange").textContent = "Change Text";
+
+}
+
+function changeTextBack() {
+	document.getElementById("clickChange").textContent = "Click Me!";
+}
